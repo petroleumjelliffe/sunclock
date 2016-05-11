eaf1887e5ee64fcbaca65a6755753fb79390c3a7
Pete Jelliffe <pete.jelliffe@gmail.com>
2016-05-10 20:37:43 -0400

    added weekly view

--- index.js
+++ index.js
@@ -93,6 +93,71 @@ app.get('/sunmoon', function(req, res) {
   res.json(arcs);
 });
 
+
+app.get('/weekly', function(req, res) {
+  //console.log(req);
+
+  function listTimes(interval, rise, set) {
+    var deltaT = interval * 60 * 1000;
+
+    //how many n minute intervals of light are there?
+    var lightIntervals = (set.getTime() - rise.getTime()) / (deltaT);
+
+    var times=[];
+
+    for (var i=0; i < lightIntervals; i++) {
+      var dateTime = new Date(rise.getTime() + (i * deltaT))
+      times.push(dateTime)
+    }
+
+    return times;
+  }
+
+  //replace times with positions at those times
+  function plotArc(fnPos, dateArray) {
+    console.log(dateArray)
+
+    return dateArray.map(function(date, index, array) {
+      console.log(date)
+      return fnPos(date, lat, lon)
+    })
+
+  }
+
+  var lat = req.query.lat || 40.6816778; //NYC
+  var lon = req.query.lon || -73.9962808; //NYC
+  var now = new Date()
+
+  // var sunTimes = sunCalc.getTimes(now, lat, lon);
+  // var moonTimes = sunCalc.getMoonTimes(now, lat, lon);
+  //
+  //
+  // var daylightTimes = listTimes(15, sunTimes.sunrise, sunTimes.sunset) //array of times to get positions for
+  //
+  // var moonlightTimes = []; //empty array of moon points
+
+  // if (!moonTimes.alwaysDown) {
+  //   if(!moonTimes.alwaysUp) {
+  //     moonlightTimes = listTimes(15, moonTimes.rise, moonTimes.set);
+  //   } else {
+  //     //if moon doesn't set get all 24 hours worth
+  //     moonlightTimes = listTimes(15, now, new Date(now.getTime() + (24 * 60 * 60 * 1000)));
+  //   }
+  // }
+
+  var times = listTimes(30,
+    new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+    new Date(now.getFullYear(), now.getMonth(), now.getDate()+7))
+console.log(times);
+
+
+  var arcs = {}
+  arcs.sun = plotArc(sunCalc.getPosition, times)
+  arcs.moon = plotArc(sunCalc.getMoonPosition, times)
+
+  res.json(arcs);
+});
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/sun', function(req, res) {
   console.log(req);
