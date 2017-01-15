a82a38c43f6951527d5b44c1b0a8011ee5d22b02
Pete Jelliffe <pete.jelliffe@gmail.com>
2017-01-15 10:10:58 -0500

    think i have some race conditions, where callbacks fire before the data is available, but seems to work

--- public/js/celestial-globe.js
+++ public/js/celestial-globe.js
@@ -1,14 +1,47 @@
 //format
 
 //CELESIAL GLOBE OBJECT DEFINITION
-var CelestialGlobe= function(spec) {
+var CelestialGlobe= function(spec, onComplete) {
   //spec is options sent to constructor
   spec.lat = spec.lat||40.6816778, //NYC
   spec.lon = spec.lon||-73.9962808, //NYC
-  spec.arcPoints = spec.arcPoints||{};
-  spec.position = spec.position||{};
   spec.timestamp = spec.timestamp||new Date();
+
+
+  spec.arcPoints = spec.arcPoints||{};
+  spec.moonArc = spec.moonArc||[];
+  spec.sunArc = spec.sunArc||[];
+  spec.position = spec.position||{}; // sun and moon right now
   console.log(spec.timestamp.toString());
+
+  //initialize position, and the sun and moon arcs
+  var getPos = function(callback) {
+    $.getJSON("sunmoon/position", spec, function(newPos) {
+      spec.position = newPos
+      console.log("updated position");
+
+      callback();
+    })
+  }
+  var getArcs = function(callback) {
+    $.getJSON("sunmoon/day", spec, function(data) {
+        spec.moonArc = data.map(function(item) {
+          return item.moon
+        })
+        spec.sunArc = data.map(function(item){
+          return item.sun
+        })
+
+        console.log("updated arcs");
+        console.log(spec.sunArc);
+
+        callback();
+    })
+  }
+  getArcs(getPos(onComplete));
+
+
+
 
   var that={};
   var labels = [
@@ -32,11 +65,11 @@ var CelestialGlobe= function(spec) {
 
   //COORDINATE functions
   //translate polar coords as seen from side
-  var xySide = function(azimuth, altitude, canvas, dAz) {
+  var xySide = function(pos, canvas, dAz) {
 
-    var x = ((azimuth+dAz) / 2 / Math.PI +0.5 )%1*canvas.width;
+    var x = ((pos.azimuth+dAz) / 2 / Math.PI +0.5 )%1*canvas.width;
     // var x = ((azimuth+dAz) / 2 / Math.PI * canvas.width) + canvas.width/2;
-    var y = canvas.height - (Math.sin(altitude) * canvas.height);
+    var y = canvas.height - (Math.sin(pos.altitude) * canvas.height);
 
     return {
       x: x,
@@ -44,12 +77,12 @@ var CelestialGlobe= function(spec) {
     };
   }
   //draw polar coords as seen from above
-  var xyTop = function(azimuth, altitude, canvas, dAz) {
+  var xyTop = function(pos, canvas, dAz) {
     dAz += Math.PI/2;  //correct for diff in 0º between the canvas and compass
 
-    var r1 = Math.cos(altitude) * canvas.width/2
-    var x = r1 * Math.cos(azimuth+dAz) + canvas.width/2
-    var y = r1 * Math.sin(azimuth+dAz) + canvas.height/2
+    var r1 = Math.cos(pos.altitude) * canvas.width/2
+    var x = r1 * Math.cos(pos.azimuth+dAz) + canvas.width/2
+    var y = r1 * Math.sin(pos.azimuth+dAz) + canvas.height/2
 
     return {
       x: x,
@@ -57,12 +90,12 @@ var CelestialGlobe= function(spec) {
     };
   }
 
-  var halfGlobe = function(azimuth, altitude, canvas, dAz) {
+  var halfGlobe = function(pos, canvas, dAz) {
     // dAz += Math.PI;  //correct for diff in 0º between the canvas and compass
 
     var r1 = -canvas.height
-    var x = r1 * Math.cos(altitude) * Math.cos(azimuth+dAz+Math.PI/2) + canvas.width/2
-    var y = r1 * Math.sin(altitude) + canvas.height
+    var x = r1 * Math.cos(pos.altitude) * Math.cos(pos.azimuth+dAz+Math.PI/2) + canvas.width/2
+    var y = r1 * Math.sin(pos.altitude) + canvas.height
 
     return {
       x: x,
@@ -79,7 +112,7 @@ var CelestialGlobe= function(spec) {
     // console.log("canvas height: " + canvas.height);
 
 
-    var p = func(sun.azimuth, sun.altitude, ctx.canvas, dAz)
+    var p = func(sun, ctx.canvas, dAz)
     var r = ctx.canvas.height/10
 
     ctx.save();
@@ -91,14 +124,16 @@ var CelestialGlobe= function(spec) {
     ctx.restore();
   }
 
-  var drawMoon = function(func, pos, src, ctx, dAz) {
+  var drawMoon = function(func, moon, src, ctx, dAz) {
+    console.log("moon");
+    console.log(moon);
     var c={}
     c.width = ctx.canvas.width;
     c.height = ctx.canvas.height;
 
-    var p = func(pos.moon.azimuth, pos.moon.altitude, ctx.canvas, dAz)
+    var p = func(moon, ctx.canvas, dAz)
     var r = c.height/10
-    var apparentAngle  = -pos.moonPhase.angle + pos.moon.parallacticAngle - Math.PI/2;
+    var apparentAngle  = -moon.angle +moon.parallacticAngle + Math.PI/2;
 
     var img = document.createElement('IMG');
     //draw image after loading
@@ -106,7 +141,7 @@ var CelestialGlobe= function(spec) {
       //assume spritesheet is made up of squares
       //h/w should be # of sprites
       var sprites = parseInt(img.naturalWidth / img.naturalHeight);
-      var spriteOffset = Math.ceil(pos.moonPhase.phase*sprites)%sprites-1
+      var spriteOffset = Math.ceil(moon.phase*sprites)%sprites-1
       console.log(sprites)
       console.log(spriteOffset)
       ctx.save()
@@ -156,19 +191,20 @@ var CelestialGlobe= function(spec) {
   }
 
   var drawArc = function(func, points, ctx, dAz) {
+    // console.log(points);
     var c={}
     c.width = ctx.canvas.width;
     c.height = ctx.canvas.height;
-
-    // console.log(points);
+    console.log("points");
+    console.log(points);
     ctx.save();
     ctx.beginPath();
     points.reduce(function(p1, p2, i, array) {
       if (p1!== null && i>0) {
-        var xy1 = func(p1.azimuth, p1.altitude, c, dAz)
+        var xy1 = func(p1, c, dAz)
         ctx.moveTo(xy1.x,xy1.y);
-
-        var xy2 = func(p2.azimuth, p2.altitude, c, dAz)
+        console.log(xy1);
+        var xy2 = func(p2, c, dAz)
         ctx.lineTo(xy2.x,xy2.y);
         ctx.stroke();
       }
@@ -210,19 +246,19 @@ var CelestialGlobe= function(spec) {
     // ctx.fillText("S", (c.width * (2 * Math.PI/2+dAz)/ (2 * Math.PI))%c.width, c.height);
     // ctx.fillText("W", (c.width * (3 * Math.PI/2+dAz)/ (2 * Math.PI))%c.width, c.height);
     labels.map(function(item, index, array){
-      var p = xySide(item.azimuth, item.altitude, ctx.canvas, dAz )
+      var p = xySide(item, ctx.canvas, dAz )
       ctx.fillText(item.label, p.x, c.height)
     })
     console.log("spec.arcPoints:");
-    // console.log(spec.arcPoints);
+    console.log(spec.arcPoints);
     //draw paths above horizon
-    drawArc(xySide, spec.arcPoints.sun, ctx, dAz);
-    drawArc(xySide, spec.arcPoints.moon, ctx, dAz);
+    drawArc(xySide, spec.sunArc, ctx, dAz);
+    drawArc(xySide, spec.moonArc, ctx, dAz);
 
     //draw sun and moon
     console.log("spec.position:");
     console.log(spec.position);
-    drawMoon(xySide, spec.position, "img/phases-sheet.png", ctx, dAz)
+    drawMoon(xySide, spec.position.moon, "img/phases-sheet.png", ctx, dAz)
     drawDisc(xySide, spec.position.sun, "#fdb813", ctx, dAz);
     // drawDisc(xySide, data.moon, "#ccc", c)
   }
@@ -260,9 +296,9 @@ var CelestialGlobe= function(spec) {
     ctx.arc(c.width/2,c.height,c.width/2,0,2*Math.PI);
     ctx.stroke();
 
-    drawArc(halfGlobe, spec.arcPoints.sun, ctx, dAz);
-    drawArc(halfGlobe, spec.arcPoints.moon, ctx, dAz);
-    drawMoon(halfGlobe, spec.position, "img/phases-sheet.png", ctx, dAz)
+    drawArc(halfGlobe, spec.sunArc, ctx, dAz);
+    drawArc(halfGlobe, spec.moonArc, ctx, dAz);
+    drawMoon(halfGlobe, spec.position.moon, "img/phases-sheet.png", ctx, dAz)
     drawDisc(halfGlobe, spec.position.sun, "#fdb813", ctx, dAz);
 
     //draw globe view
@@ -293,12 +329,12 @@ var CelestialGlobe= function(spec) {
     ctx.stroke();
 
 
-    drawArc(xyTop, spec.arcPoints.sun, ctx, dAz);
-    drawArc(xyTop, spec.arcPoints.moon, ctx, dAz);
+    drawArc(xyTop, spec.sunArc, ctx, dAz);
+    drawArc(xyTop, spec.moonArc, ctx, dAz);
 
     drawShadow(spec.position.sun,"img/shadow.png", ctx, topCanvas[0], dAz)
 
-    drawMoon(xyTop, spec.position, "img/phases-sheet.png", ctx, dAz)
+    drawMoon(xyTop, spec.position.moon, "img/phases-sheet.png", ctx, dAz)
     drawDisc(xyTop, spec.position.sun, "#fdb813", ctx, dAz);
     // drawDisc(xyTop, data.moon, "#ccc", topCanvas[0])
   }
@@ -336,8 +372,21 @@ var CelestialGlobe= function(spec) {
     spec.timestamp = new Date(string)
   }
 
-  that.setTime = function(hours, minutes) {
+  that.updateDay = function(timestamp, callback) {
+    var newDate = new Date(timestamp);
+
+    if (newDate.toDateString() !== spec.timestamp.toDateString()) {
+      spec.timestamp = new Date(timestamp);
+
+      getArcs(getPos(callback))
+    }
+  }
+
+  that.updateTime = function(hours, minutes, callback) {
     spec.timestamp.setHours(hours, minutes)
+
+    getPos(callback)
+
   }
 
   that.getTime = function() {
